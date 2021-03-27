47d0f1e9832c1ef82f77dd87a386187abd9e39ec
Imran <arkateimran11@gmail.com>
2021-03-27 15:43:09 +0530

    min element'

---
+++ src/start/MinElement.java
@@ -0,0 +1,29 @@
+package start;
+
+public class MinElement {
+	
+	public static void main(String[] args) {
+
+	
+	int[] a = new int[6];
+	a[0] = 11;
+	a[1] = 3;
+	a[2] = 10;
+	a[3] = 13;
+	a[4] = 96;
+	a[5] = 24;
+
+	int min = a[0];
+	for (int i = 0; i < a.length; i++) {
+		if(a[i] < min) {
+			min = a[i];
+			
+		}
+		
+	}
+	
+	 System.out.println("Minimum value in the array is:: " + min);
+	
+	}
+
+}
