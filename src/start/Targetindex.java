26a67b3ee0bee4327cb4b84016bab90b48dade3b
mubin <mubinparkar18@gmail.com>
2021-03-30 00:10:12 +0530

    Target Problem

---
+++ src/start/Targetindex.java
@@ -0,0 +1,26 @@
+package start;
+
+public class Targetindex {
+	public static void main(String args[])
+	{
+		int[]a= {19,26,33,47,25,42};
+		int target=33;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i] == target)
+			{
+				System.out.println(i + " target index is " + a[i]);
+				
+					
+				
+				
+				
+				
+			}
+		}
+			
+		}
+		
+	}
+
+
