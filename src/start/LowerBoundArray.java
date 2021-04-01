7f1ad6ab4a73863d2c9ed1263b74cb211ab19d2e
anitahcu <anitatiwaric@gmail.com>
2021-04-01 07:25:17 +0530

    upperbound

--- src/start/LowerBoundArray.java
+++ src/start/LowerBoundArray.java
@@ -1,24 +1,40 @@
 package start;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class LowerBoundArray {
     public static void main(String[] args) {
 
         int[] a = {16, 19, 20, 23, 45, 56, 78, 90, 96, 100};
-        int l = 0, r = a.length;
 
-        static int LowerBound(int arr[],int x){ // x is the target value
+        lowerBound(a, 20);
+        //  System.out.println(r);
+    }
 
-            while (x >= 0) {
+    static int lowerBound(int arr[], int x) { // x is the target value
+        int r = arr.length;
+        int l = 0;
+        while (l >=r) {
 
-                int mid = (l + r) / 2;
 
-                if (a[mid] >= x)
-                    r = mid;
-                else l = mid;
+            int mid = (l + r) / 2;
+
+            if (arr[mid] >= x)
+                r = mid;
+            else {
+                l = mid;
             }
-            System.out.println("lower Bound="+r);
-
-
+            System.out.println(mid);
         }
+        return r;
 
-    }}
+
+
+    }
+
+
+
+
+
+}
---
+++ src/start/Upperbound.java
@@ -0,0 +1,32 @@
+package start;
+
+public class Upperbound {
+
+    public static void main(String[] args) {
+
+        int[] a = { 2, 3, 4, 8, 10, 13, 17, 19, 23 };
+
+        int key = 11;
+        int start = 0;
+        int end = a.length - 1;
+        int result = 0;
+
+        while (start <= end) {
+            System.out.println("Start : " + start + " | End : " + end);
+            int mid = (start + end) / 2;
+
+            if (a[mid] > key) {
+                result = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+
+        }
+        System.out.println(result);
+
+    }
+}
+
+
+
