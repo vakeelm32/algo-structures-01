30e05dca58bd288f4975da126f4484fe9fb28a9f
Imran <arkateimran11@gmail.com>
2021-04-03 09:25:39 +0530

    commit

--- src/start/LowerBound.java
+++ src/start/LowerBound.java
@@ -3,17 +3,27 @@ package start;
 public class LowerBound {
 	public static void main(String[] args) {
 
-		int[] a = { 1, 3, 4, 6, 10, 11, 14, 15 };
+		int[] a = { 2, 3, 4, 8, 10, 13, 17, 19, 23 };
 
-		int target = 4;
-		for (int i = 0; i < a.length; i++) {
-			if (a[i] > target) {
-				System.out.println("Lower bound of target is at index:" + i);
-				break;
+		int key = 3;
+		int start = 0;
+		int end = a.length - 1;
+		int result = 0;
 
+		while (start <= end) {
+			System.out.println("Start : " + start + " | End : " + end);
+			int mid = (start + end) / 2;
+
+			if (a[mid] >= key) {
+				result = mid;
+				end = mid - 1;
+			} else {
+				start = mid + 1;
 			}
 
 		}
-	}
+		System.out.println(result);
 
+	}
 }
+
