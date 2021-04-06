91cb69603809bd860fff1dda550924432198b005
Imran <arkateimran11@gmail.com>
2021-04-06 12:58:11 +0530

    sub array

---
+++ src/start/SubElements.java
@@ -0,0 +1,19 @@
+package start;
+
+public class SubElements {
+	public static void main(String[] args) {
+
+		int[] a = { 3, 1, 5, 4, 8, 9, 3, 5, 6, 1 };
+		int k = 4;
+
+		for (int i = 0; i < a.length - k; i++) {
+			for (int j = i; j < i + k; j++) {
+				System.out.println(a[j] + ",");
+
+			}
+			System.out.println(" ");
+
+		}
+
+	}
+}
