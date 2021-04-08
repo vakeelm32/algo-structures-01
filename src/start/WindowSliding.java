2a705d597841997444965c0f768943a6d9fd7bfc
Imran <arkateimran11@gmail.com>
2021-04-08 09:40:05 +0530

    window sliding

---
+++ src/start/WindowSliding.java
@@ -0,0 +1,24 @@
+package start;
+
+public class WindowSliding {
+
+	public static void main(String[] args) {
+		int[] a = { 12, 14, 11, 7, 16, 13, 10, 5, 10, 30 };
+		int k = 4;
+
+		int windowSum = 0;
+		for (int i = 0; i < k; i++) {
+			windowSum = windowSum + a[i];
+		}
+		System.out.println("Window sum " + windowSum);
+
+		for (int j = 1; j < a.length - k + 1; j++) {
+
+			int elementToBeDeleted = a[j - 1];
+			int elementToBeAdded = a[j + k - 1];
+			windowSum = windowSum + elementToBeAdded - elementToBeDeleted;
+			System.out.println("Window Sum " + windowSum);
+		}
+
+	}
+}
