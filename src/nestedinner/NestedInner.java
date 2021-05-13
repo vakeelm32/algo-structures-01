633560a8ce9071b3ac415fb51b07dc76fda247d9
mubin <mubinparkar18@gmail.com>
2021-05-13 06:33:38 +0530

    InnerClasses

---
+++ src/nestedinner/NestedInner.java
@@ -0,0 +1,28 @@
+package nestedinner;
+class Outer
+{
+	
+	int x=10;
+	class Inner{
+		int y=20;
+		public void innerDisplay() {
+			System.out.println(x+" "+y);
+			
+		}
+	}
+	public void outerDisplay() {
+		Inner i=new Inner();
+		i.innerDisplay();
+		System.out.println(i.y);
+		
+	}
+	}
+
+
+public class NestedInner {
+	public static void main (String[] args) {
+		Outer.Inner oi=new Outer().new Inner();
+		oi.innerDisplay();
+	}
+
+}
