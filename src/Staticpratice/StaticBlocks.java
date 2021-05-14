a291ec64587f4a7a307640d47591a361fe3f984e
mubin <mubinparkar18@gmail.com>
2021-05-14 22:48:12 +0530

    Static problems

---
+++ src/Staticpratice/StaticBlocks.java
@@ -0,0 +1,21 @@
+package Staticpratice;
+class Testpractice
+{
+	static {
+		System.out.println("Block 1");
+	}
+	static {
+		System.out.println("Block 2");
+	}
+}
+	public class StaticBlocks {
+	public static void main(String args[]) {
+		System.out.println("Main");
+	Testpractice t=new Testpractice();
+	
+	}
+	
+
+	
+
+}
