bc3601b0154cacdaa388b6942bebacb6d165e48b
Imran <arkateimran11@gmail.com>
2021-05-29 12:19:14 +0530

    java section

---
+++ src/mathclass/MathclassPract.java
@@ -0,0 +1,26 @@
+package mathclass;
+
+public class MathclassPract {
+	public static void main(String[] args) {
+		System.out.println("Absolut:");
+		System.out.println(Math.abs(-123));
+		
+		System.out.println("Maximum:");
+		System.out.println(Math.max(7,5));
+		
+		System.out.println("Minimum:");
+		System.out.println(Math.min(7,5));
+		
+		System.out.println("CUbe root:");
+		System.out.println(Math.cbrt(64));
+		
+		System.out.println("Square ROot:");
+		System.out.println(Math.sqrt(100));
+		
+		System.out.println("Power:");
+		System.out.println(Math.pow(2,8));
+		
+		
+	}
+
+}
