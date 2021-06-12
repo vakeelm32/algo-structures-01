cc2c1a17aa80723f45827361c6b6bd8e56e15ccf
Imran <arkateimran11@gmail.com>
2021-06-12 20:05:10 +0530

    overriding

---
+++ src/inheritance/Overriding.java
@@ -0,0 +1,38 @@
+package inheritance;
+
+class TV {
+	public void switchOn() {
+		System.out.println("TV is switched on");
+	}
+
+	public void changeChannel() {
+		System.out.println("TV channel is changed");
+	}
+}
+
+class SmartTV extends TV {
+	public void switchOn() {
+		System.out.println("SmartTV is switched on");
+	}
+
+	public void changeChannel() {
+		System.out.println("SmartTV channel is changed");
+	}
+
+	public void browse() {
+		System.out.println("SmartTV browsing");
+	}
+
+}
+
+public class Overriding {
+	public static void main(String[] args) {
+		TV t = new SmartTV();
+		t.changeChannel();
+		t.switchOn();
+		//t.browse();
+		
+		
+	}
+
+}
