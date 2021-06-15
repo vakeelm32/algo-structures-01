19a5898d22a92601cbe55aec71317b6fcba0edda
Imran <arkateimran11@gmail.com>
2021-06-15 21:11:42 +0530

    instanceof

---
+++ src/inheritance/PrintableDemo.java
@@ -0,0 +1,42 @@
+package inheritance;
+
+interface Printable {
+
+}
+
+class A implements Printable {
+	public void a() {
+		System.out.println("a method");
+
+	}
+
+}
+
+class B implements Printable {
+	public void b() {
+		System.out.println("b method");
+	}
+}
+
+class Call {
+	void invoke(Printable p) {
+		if (p instanceof A) {
+			A a = (A) p;
+			a.a();
+		}
+		if (p instanceof B) {
+			B b = (B) p;
+			b.b();
+		}
+
+	}
+}
+
+public class PrintableDemo {
+	public static void main(String[] args) {
+		Printable p = new B();
+		Call c = new Call();
+		c.invoke(p);
+	}
+
+}
