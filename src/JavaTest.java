2e099b7b0db078ab1b106670d856aba31d63d7f9
techvakeelm32@gmail.com <AmericA3232>
2021-06-30 23:02:30 +0530

    Commit

--- src/JavaTest.java
+++ src/JavaTest.java
@@ -1,9 +1,11 @@
 
-public class JavaTest  {
-	public static void main(String[] args) {
-		System.out.println("sd");
-	}
-	
+public class JavaTest {
+    public static void main(String[] args) {
+        int a = 100000;
+        int b = 100000;
+        System.out.println(a + b);
+    }
+
 }
 
 
--- src/multithreading/ThreadDemo3.java
+++ src/multithreading/ThreadDemo3.java
@@ -1,19 +1,42 @@
 package multithreading;
 
 public class ThreadDemo3 {
-    public static void main(String[] args) {
-        foo();
+    public static void main(String[] args) throws InterruptedException {
+
+        PrintSumNumbers obj = new PrintSumNumbers();
+
+        Thread t1 = new Thread(obj);
+        t1.setName("T1");
+
+        Thread t2 = new Thread(obj);
+        t2.setName("T2");
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        int totalSum = obj.totalSum;
+        System.out.println("******** Total Sum *******  => " + totalSum);
+
+    }
+}
+
+class PrintSumNumbers implements Runnable {
+
+    public int totalSum = 0;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " Number is " + i);
+            updateCounter();
+        }
     }
 
-    private static void foo() {
-        bar();
-    }
-
-    private static void bar() {
-        zoo();
-    }
-
-    private static void zoo() {
+    private synchronized void updateCounter() {
+        totalSum = totalSum + 1;
     }
 }
 
