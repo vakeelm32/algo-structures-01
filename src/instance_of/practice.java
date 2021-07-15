bb564ff1aa1f60beddde33ec3abb5efaf56622fc
mubin <mubinparkar18@gmail.com>
2021-07-15 20:38:46 +0530

    arraylist

--- src/instance_of/practice.java
+++ src/instance_of/practice.java
@@ -1,42 +1,11 @@
 package instance_of;
 
-interface Printable {
-
-}
-
-class A implements Printable {
-	public void a() {
-		System.out.println("a method");
-
+public class practice{
+	public static void main(String[] args)
+	{
+		int A[]={2,5,1,0,10,8};
+		
+		for(int i=A.length-1;i>=0;i--)
+	         System.out.print( A[i] + "  ");
 	}
-
-}
-
-class B implements Printable {
-	public void b() {
-		System.out.println("b method");
-	}
-}
-
-class Call {
-	void invoke(Printable p) {
-		if (p instanceof A) {
-			A a = (A) p;
-			a.a();
-		}
-		if (p instanceof B) {
-			B b = (B) p;
-			b.b();
-		}
-
-	}
-}
-
-public class practice {
-	public static void main(String[] args) {
-		Printable p = new B();
-		Call c = new Call();
-		c.invoke(p);
-	}
-
 }
